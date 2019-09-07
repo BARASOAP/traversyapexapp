49cd3ee153e6fded49cfc71081d6a0bec354b8e7
BARASOAP <niel.j.barasona@gmail.com>
2019-09-07 04:58:18 -0400

    Add Morgan to Development Environment Logging

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use('/api/v1/profile', require('./routes/profile'))
 
 const port = process.env.PORT || 8000;
